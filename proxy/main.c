cb14b51a4aa34b69eaa61bed09c18c1ab34164e7
liuzheng <edisonlz1@163.com>
2013-08-07 16:00:35 +0800

    更新带来端口

--- proxy/main.c
+++ proxy/main.c
@@ -105,7 +105,7 @@ int connect_remote(char *server,int port){
     
 
     sin.sin_family = AF_INET;
-    sin.sin_port = htons(9090);
+    sin.sin_port = htons(80);
     inet_pton(AF_INET,"10.103.13.18",&sin.sin_addr);
     
     if (connect(remote, (struct sockaddr*) &sin, sizeof(sin))) {
